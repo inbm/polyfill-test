bc23ff59d00d946b12084019f80e68f03d442b38
inbm <inbm@naver.com>
2022-04-20 10:06:05 +0900

    commit

--- config-overrides.js
+++ config-overrides.js
@@ -1,30 +1,30 @@
 const { setWebpackTarget, override, addBabelPresets, addBabelPlugins } = require("customize-cra");
 
 module.exports = override(
-   setWebpackTarget(['web', 'es5']),
+   // setWebpackTarget(['web', 'es5']),
 
-   addBabelPresets(
-      [
-         "@babel/preset-env",
-         {
-            "targets": {"ie": "11"}, 
-            corejs: 3,
-         },
+   // addBabelPresets(
+   //    [
+   //       "@babel/preset-env",
+   //       {
+   //          "targets": {"ie": "11"}, 
+   //          corejs: 3,
+   //       },
          
-      ],
+   //    ],
       
       
-   ),
+   // ),
 
-   addBabelPlugins(
-      [
-         [
-           '@babel/plugin-transform-runtime',
-           {
-             "corejs": 3,
-           },
-         ]
-      ]
-   )
+   // addBabelPlugins(
+   //    [
+   //       [
+   //         '@babel/plugin-transform-runtime',
+   //         {
+   //           "corejs": 3,
+   //         },
+   //       ]
+   //    ]
+   // )
 
 )
